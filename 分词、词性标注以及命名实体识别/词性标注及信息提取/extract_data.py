12ea7fb2ce30509935d5ac0de2e6c34b81b574dc
JustLonggg <1292604234@qq.com>
2019-04-19 16:35:43 +0800

    chapter3

---
+++ 分词、词性标注以及命名实体识别/词性标注及信息提取/extract_data.py
@@ -0,0 +1,12 @@
+#encoding=utf8
+import jieba
+import re
+from tokenizer import seg_sentences
+
+fp = open('text.txt','r',encoding='utf-8')
+fout = open('out.txt','w',encoding='utf-8')
+for line in fp:
+    line = line.strip()
+    if len(line) > 0:
+        fout.write(' '.join(seg_sentences(line)) + '\n')
+fout.close()
